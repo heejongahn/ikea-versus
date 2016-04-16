1cf43a7142d99fe147fe1d1448a309a577b0ac53
Heejong Ahn <heejongahn@gmail.com>
2016-04-16 22:29:29 +0200

    added AddCardButton component

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Greeting from './components/Greeting';
 
 import 'script!jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+let AddCardButton = React.createClass({
+  render: () => {
+    return (
+      <span className="glyphicon glyphicon-plus"></span>
+    );
+  }
+});
+
 ReactDom.render(
-  React.createElement(Greeting),
+  React.createElement(AddCardButton),
   document.getElementById('content')
 );
