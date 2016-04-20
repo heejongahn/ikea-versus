74cc7f03a5c959c9e3b7bb2ddbb69ab07430c0b8
Heejong Ahn <heejongahn@gmail.com>
2016-04-20 12:55:57 +0200

    added basic styling

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ReactDom from 'react-dom';
 import 'script!jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.styl'
 
 let AppContainer = React.createClass({
   getInitialState: () => {
@@ -16,7 +17,7 @@ let AppContainer = React.createClass({
   render: function() {
     let products = [];
     return (
-      <div className="container">
+      <div className="container app-container">
         {this.state.items.map(item => item)}
         <AddCardButton handleAddCard={this.handleAddCard}/>
       </div>
@@ -27,7 +28,9 @@ let AppContainer = React.createClass({
 let ProductCard = React.createClass({
   render: () => {
     return (
-      <h1>Product Card</h1>
+      <div className="product-card">
+        <h1>Product Card</h1>
+      </div>
     )
   }
 });
